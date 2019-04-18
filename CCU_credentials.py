7b21bbdaeef620de3b0264ff4d49821c72e97032
DimsterTim <DimsterTim@users.noreply.github.com>
2019-04-18 14:42:31 +0300

    Update CCU_credentials.py

--- CCU_credentials.py
+++ CCU_credentials.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Parameters for CCU825_GetState.py
 
-CCU_Pass = '123456Di'
-CCU_Login = 'Dimster@867861030048455'
+CCU_Pass = 'Pass'
+CCU_Login = 'Login'
 Request_Rate = 60 # в секундах
 Server_Port = 7788
